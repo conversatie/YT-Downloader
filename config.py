4f5803f9913135174ec3a5b6ed9fc4c364b09635
conversatie <florea.ionut@outlook.com>
2021-08-11 17:44:21 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 import  os
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN")
-APP_ID = int(os.environ.get("API_ID"))
-API_HASH = os.environ.get("API_HASH")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "1846094151:AAHt0xygishhlcMErZKZO9gbgYRW8kyD_mQ")
+APP_ID = int(os.environ.get("API_ID", "7378520"))
+API_HASH = os.environ.get("API_HASH", "6e04739d66a87f305207247d39875887")
 
 youtube_next_fetch = 0  # time in minute
 
